c9c993a2c4e12230a97d22f647a6f78bc451ceed
Maruf Billah <marufbillah.dev@gmail.com>
2026-06-19 15:40:52 +0600

    feat(server): implement initial Express server setup with CORS

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is cooking!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is cooking on port ${port}`);
+});
