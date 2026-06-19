58ce1a5a2ec50a7c7ba44304e1b4d3aeeaddffca
Maruf Billah <marufbillah.dev@gmail.com>
2026-06-19 16:34:45 +0600

    feat(recipes): implement createRecipe function for adding new recipes

---
+++ controllers/recipes.controller.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+const { database } = require("../config/db");
+const { ObjectId } = require("mongodb");
+
+const recipesCollection = database.collection("recipes");
+const usersCollection = database.collection("user");
+
+// Create a new recipe
+const createRecipe = async (req, res) => {
+  try {
+    const {
+      recipeName,
+      description,
+      category,
+      cuisine,
+      difficulty,
+      prepTime,
+      servings,
+      isPremium,
+      price,
+      imageUrl,
+      ingredients,
+      steps,
+      userId,
+    } = req.body;
+
+    if (
+      !recipeName ||
+      !description ||
+      !category ||
+      !cuisine ||
+      !difficulty ||
+      !prepTime ||
+      !servings ||
+      !isPremium ||
+      !price ||
+      !imageUrl ||
+      !ingredients ||
+      !steps ||
+      !userId
+    ) {
+      return res.status(400).json({ message: "Missing required fields!" });
+    }
+
+    const newRecipe = {
+      recipeName,
+      description,
+      category,
+      cuisine,
+      difficulty,
+      prepTime,
+      servings,
+      isPremium,
+      price,
+      imageUrl,
+      ingredients,
+      steps,
+      userId: new ObjectId(payload.userId),
+      createdAt: new Date(),
+    };
+
+    const result = await recipesCollection.insertOne(newRecipe);
+
+    await usersCollection.updateOne(
+      { _id: new ObjectId(userId) },
+      { $inc: { recipes: 1 } },
+    );
+
+    res.status(201).json({
+      message: "Recipe created successfully!",
+      recipeId: result.insertedId,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating recipe!" });
+  }
+};
+
+module.exports = { createRecipe };
