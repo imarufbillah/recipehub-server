630567d0ce4241ddcdaea6a01060eda289bd33a4
Maruf Billah <marufbillah.dev@gmail.com>
2026-06-19 18:46:38 +0600

    feat(recipes): add route to get recipes by user ID

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createRecipe } = require("../controllers/recipes.controller");
+const {
+  createRecipe,
+  getRecipesByUserId,
+} = require("../controllers/recipes.controller");
 
 router.post("/", createRecipe);
+router.get("/:userId", getRecipesByUserId);
 
 module.exports = router;
