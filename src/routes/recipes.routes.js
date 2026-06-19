5b58740d06be7059aa9e3bd756677cc3bacb11a7
Maruf Billah <marufbillah.dev@gmail.com>
2026-06-20 00:04:36 +0600

    feat(recipes): add route to update recipes by recipe ID

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   createRecipe,
   getRecipesByUserId,
+  updateRecipe,
 } = require("../controllers/recipes.controller");
 
 router.post("/", createRecipe);
 router.get("/:userId", getRecipesByUserId);
+router.patch("/:recipeId", updateRecipe);
 
 module.exports = router;
