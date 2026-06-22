f221fb30f146dea59808e7e812bca9cedebd34b1
Maruf Billah <marufbillah.dev@gmail.com>
2026-06-22 18:55:27 +0600

    refactor(reports): improve createReport logic with error handling and response status

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -9,6 +9,7 @@ const reportsCollection = database.collection("reports");
 const createReport = async (req, res) => {
   try {
     const { userId, recipeId, reason, additionalContext = "" } = req.body;
+
     const newReport = {
       userId: new ObjectId(userId),
       recipeId: new ObjectId(recipeId),
@@ -18,8 +19,13 @@ const createReport = async (req, res) => {
       createdAt: new Date(),
     };
 
-    await reportsCollection.insertOne(newReport);
-    res.json({ message: "Report created successfully!" });
+    const result = await reportsCollection.insertOne(newReport);
+
+    if (!result.acknowledged) {
+      return res.status(500).json({ message: "Error creating report!" });
+    }
+
+    res.status(201).json({ message: "Report created successfully!" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error creating report!" });
